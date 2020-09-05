1dc46c5eadb83456ac989add8fe21b53cbb6ca66
juniordugue <jrdugue@gmail.com>
2020-09-04 22:10:51 -0400

    finished the first 5

--- src/03_modules.py
+++ src/03_modules.py
@@ -11,12 +11,16 @@ import sys
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
 
+
+
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
 
+print('version of Python', sys.version_info)
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
@@ -24,8 +28,14 @@ import os
 # Print the current process ID
 # YOUR CODE HERE
 
+print('the current process ID', os.getpid())
+
 # Print the current working directory (cwd):
 # YOUR CODE HERE
 
+print('current working directory', os.getcwd())
+
 # Print out your machine's login name
 # YOUR CODE HERE
+
+print('machine login name', os.getlogin())
